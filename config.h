d4ad1331f48012f95e7689a9b5d2b295ab8805e0
Eid Alkhaldi <eid.alkhaldi@gmail.com>
2021-01-18 23:23:58 -0500

    changed some keybindings

--- config.h
+++ config.h
@@ -214,13 +214,12 @@ static Key keys[] = {
 	{ MODKEY,			XK_F2,		spawn,		SHCMD("tutorialvids") },
 	{ MODKEY,			XK_F3,		spawn,		SHCMD("displayselect") },
 	{ MODKEY,			XK_F4,		spawn,		SHCMD(TERMINAL " -e pulsemixer; kill -44 $(pidof dwmblocks)") },
-	{ MODKEY,			XK_F5,		spawn,		SHCMD("emacsclient -ne (make-capture-frame)") },
+	{ MODKEY,			XK_F5,		spawn,		SHCMD(TERMINAL" -e emacsclient -ne '(make-capture-frame)'") },
 	/* { MODKEY,			XK_F5,		xrdb,		{.v = NULL } }, */
 	{ MODKEY,			XK_F6,		spawn,		SHCMD("torwrap") },
 	{ MODKEY,			XK_F7,		spawn,		SHCMD("td-toggle") },
 	{ MODKEY,			XK_F8,		spawn,		SHCMD("mw sync") },
 	/* { MODKEY,			XK_F9,		spawn,		SHCMD("dmenumount") },
-	{ MODKEY,			XK_F10,		spawn,		SHCMD("dmenuumount") },*/
 	{ MODKEY,			XK_F9,		spawn,		SHCMD("mytrans") },
 	{ MODKEY,			XK_F10,		spawn,		SHCMD("dmenuumount") },
 	/* { MODKEY,			XK_F11,		spawn,		SHCMD("mpv --no-cache --no-osc --no-input-default-bindings --input-conf=/dev/null --title=webcam $(ls /dev/video[0,2,4,6,8] | tail -n 1)") }, */
