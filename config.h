82edd0609e2a58b237c03b39d7659d2732f09c04
Eid Alkhaldi <eid.alkhaldi@gmail.com>
2022-01-11 23:12:34 -0500

    Tue Jan 11 11:12:34 PM EST 2022

--- config.h
+++ config.h
@@ -42,7 +42,7 @@ static Sp scratchpads[] = {
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { "work", "stocks", "dev", "email", "browsing", "6", "7", "8", "misc" };
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -51,6 +51,8 @@ static const Rule rules[] = {
 	*/
 	/* class    instance      title       	 tags mask    isfloating   isterminal  noswallow  monitor */
 	{ "Gimp",     NULL,       NULL,       	    1 << 8,       0,           0,         0,        -1 },
+	{ "discord",  NULL,       NULL,       	    2 ,       0,           0,         0,        -1 },
+	{ "Thunderbird",  NULL,   NULL,       	    1 << 3 ,       0,           0,         0,        -1 },
 	{ TERMCLASS,   NULL,       NULL,       	    0,            0,           1,         0,        -1 },
 	{ NULL,       NULL,       "Event Tester",   0,            0,           0,         1,        -1 },
 	{ NULL,      "spterm",    NULL,       	    SPTAG(0),     1,           1,         0,        -1 },
